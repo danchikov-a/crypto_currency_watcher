107d58afe4415aa22176ff76cf4cb7ecc827b462
danchikov_a <danchikovartem@gmail.com>
2022-07-13 20:40:28 +0300

    Created CoinServiceTest

---
+++ src/test/java/service/CoinServiceTest.java
@@ -0,0 +1,20 @@
+package service;
+
+import com.idfinance.service.CoinService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CoinServiceTest {
+    @Mock
+    private CoinService coinService;
+
+    @Test
+    public void shouldReturnList() {
+        assertNotNull(coinService.getCoinList());
+    }
+}
